5ed57d23ee6b7e062cf696c06faf8b69c4ac6b38
camap <ng.thule2005@gmail.com>
2015-06-16 22:26:26 +0700

    homepage, global header

---
+++ skin/frontend/rwd/beut/scss/config.rb
@@ -0,0 +1,14 @@
+# note: this should never truly be refernced since we are using relative assets
+add_import_path "../../../rwd/default/scss"
+http_path = "/skin/frontend/rwd/beut/"
+css_dir = "../css"
+sass_dir = "../scss"
+images_dir = "../images"
+javascripts_dir = "../js"
+relative_assets = true
+
+
+output_style = :expanded
+environment = :development
+sourcemap = true
+extensions_dir = "bower_components"
