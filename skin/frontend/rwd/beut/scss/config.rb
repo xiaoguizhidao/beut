a7087b6a1d2ca4a8bdd497e8e20b26e7cb68befd
camap <ng.thule2005@gmail.com>
2015-08-15 15:10:27 +0700

    fix menu

--- skin/frontend/rwd/beut/scss/config.rb
+++ skin/frontend/rwd/beut/scss/config.rb
@@ -1,13 +1,12 @@
 # note: this should never truly be refernced since we are using relative assets
 add_import_path "../../../rwd/default/scss"
 http_path = "/skin/frontend/rwd/beut/"
+
 css_dir = "../css"
 sass_dir = "../scss"
 images_dir = "../images"
 javascripts_dir = "../js"
 relative_assets = true
 
-
 output_style = :expanded
 environment = :development
-
